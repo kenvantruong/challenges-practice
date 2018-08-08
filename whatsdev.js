5ae199d5fb82ab4730e3f2dc755dfaaa6417bb58
Ken Truong <kenvantruong@gmail.com>
2018-08-08 12:52:01 -0700

    1

--- whatsdev.js
+++ whatsdev.js
@@ -1,9 +1,19 @@
-let list = [20,20,20,20,20];
-
-let answer = 0;
-
-list.forEach(function(e){
-  answer += e;
-});
-
-console.log(answer);
+// #1 Reverse a String
+// #2 Factorialize
+// #3 Palindrome Check
+// #4 Find Longest Word
+// #5 Titlecase
+// #6 Largest Numbers in
+// #7 Confirm the
+// #8 Repeat String Num Times
+// #9 Truncate a String
+// #10 Chunky Monkey
+// #11 Slasher
+// #12 Mutations
+// #13 Falsy Bouncer
+// #14 Seek and Destroy
+// #15 Where do I belong
+// #16 Caesars Cipher
+// #17 Sum All the Numbers in
+// #18 DiffArray
+// #19 Roman Numeral
