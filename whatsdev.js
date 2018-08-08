33ad516c861729a155ef1cf32018bd9f6f05ec2e
Ken Truong <kenvantruong@gmail.com>
2018-08-08 13:51:19 -0700

    palindrome finished

--- whatsdev.js
+++ whatsdev.js
@@ -27,6 +27,15 @@ console.log(factorialize(5));
 /* ------------------------- */
 
 // #3 Palindrome Check
+function palindrome(str) {
+  var reversed = str.split('').reverse().join('');
+
+  if(reversed === str) 
+  return true
+  return false;
+}
+
+console.log(palindrome("racecar"))
 /* ------------------------- */
 
 // #4 Find Longest Word
