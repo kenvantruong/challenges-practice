89497546c8cd8e883857fe1399ffc2b328aca9ee
Ken Truong <kenvantruong@gmail.com>
2018-08-07 23:56:18 -0700

    Update SonLuChallenge.js

--- SonLuChallenge.js
+++ SonLuChallenge.js
@@ -16,3 +16,25 @@ sonLuChallenge([51,28,73,44,65])
 
 ---------------------------------
 ---------------------------------
+
+//Highest Number
+
+var list = [10,239,8,17,6,5,41,3,2,1]
+
+let highestNum = 0;
+
+list.forEach(function(e){
+  if (e > highestNum){
+    highestNum = e;
+    console.log(e);
+  }
+})
+
+
+
+console.log(highestNum);
+
+
+
+---------------------------------
+---------------------------------
