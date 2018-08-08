e3c8e277c06f6b55ca4c07af7179b0ee05cf4dea
Ken Truong <kenvantruong@gmail.com>
2018-08-08 13:15:41 -0700

    finished reverseAString

--- whatsdev.js
+++ whatsdev.js
@@ -1,19 +1,70 @@
 // #1 Reverse a String
+function reverseAString(str){
+  var answer = '';
+
+  for(var i = str.length - 1; i >= 0; i--){
+    answer += str[i]
+  }
+
+  return answer;
+}
+console.log(reverseAString('Ken'));
+
+/* ------------------------- */
+
 // #2 Factorialize
+function factorialize(){
+
+}
+/* ------------------------- */
+
 // #3 Palindrome Check
+/* ------------------------- */
+
 // #4 Find Longest Word
+/* ------------------------- */
+
 // #5 Titlecase
+/* ------------------------- */
+
 // #6 Largest Numbers in
+/* ------------------------- */
+
 // #7 Confirm the
+/* ------------------------- */
+
 // #8 Repeat String Num Times
+/* ------------------------- */
+
 // #9 Truncate a String
+/* ------------------------- */
+
 // #10 Chunky Monkey
+/* ------------------------- */
+
 // #11 Slasher
+/* ------------------------- */
+
 // #12 Mutations
+/* ------------------------- */
+
 // #13 Falsy Bouncer
+/* ------------------------- */
+
 // #14 Seek and Destroy
+/* ------------------------- */
+
 // #15 Where do I belong
+/* ------------------------- */
+
 // #16 Caesars Cipher
+/* ------------------------- */
+
 // #17 Sum All the Numbers in
+/* ------------------------- */
+
 // #18 DiffArray
+/* ------------------------- */
+
 // #19 Roman Numeral
+/* ------------------------- */
