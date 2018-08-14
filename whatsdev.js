0aae09a4f1a91358fae66890f2eb868f7c6c7abf
Ken Truong <kenvantruong@gmail.com>
2018-08-14 12:36:44 -0700

    added qa bonus round answer

--- whatsdev.js
+++ whatsdev.js
@@ -104,46 +104,88 @@ for(var k = 0; k < 3; k++) {
 
 // -------------------------------------------------
 
-// #21 Bonus Round
+// -------------------------------------------------
+// #21 Bonus Round 1
+// Extract name, price values and populate them into an array
+// @param {Array} array of objects
+// @return {Array} array of arrays
+// function renderInventory(inventory) {
+  //your code here
+// }
+// var currentInventory = [
+// {
+// name: 'Brunello Cucinelli',
+// shoes: [
+//   {name: 'tasselled black low-top lace-up', price: 1000},
+//   {name: 'tasselled green low-top lace-up', price: 1100},
+//   {name: 'plain beige suede moccasin', price: 950},
+//   {name: 'plain olive suede moccasin', price: 1050}
+// ]
+// },
+// {
+// name: 'Gucci',
+// shoes: [
+//   {name: 'red leather laced sneakers', price: 800},
+//   {name: 'black leather laced sneakers', price: 900}
+// ]
+// }
+// ];
+// Output
+// [ [ 'Brunello Cucinelli', 'tasselled black low-top lace-up', 1000 ],
+// [ 'Brunello Cucinelli', 'tasselled green low-top lace-up', 1100 ],
+// [ 'Brunello Cucinelli', 'plain beige suede moccasin', 950 ],
+// [ 'Brunello Cucinelli', 'plain olive suede moccasin', 1050 ],
+// [ 'Gucci', 'red leather laced sneakers', 800 ],
+// [ 'Gucci', 'black leather laced sneakers', 900 ] ]
+// console.log(renderInventory(currentInventory))
+
+
+
+// -------------------------------------------------
+
+// -------------------------------------------------
+
+
+// Bonus Round - ANSWER
 
 // Extract name, price values and populate them into an array
 // @param {Array} array of objects
 // @return {Array} array of arrays
-function renderInventory(inventory) {
+// function renderInventory(inventory) {
   //your code here
   
-  let list = [];
+//   let list = [];
   
-  inventory.forEach(function(e) {
+//   inventory.forEach(function(e) {
     
     
-    e.shoes.forEach(function(x) {
+//     e.shoes.forEach(function(x) {
     
-        list.push([e.name, x.name, x.price]);
+//         list.push([e.name, x.name, x.price]);
         
-    });
-  });
-  return list;
+//     });
+//   });
+//   return list;
   
-}
-var currentInventory = [
-  {
-    name: 'Brunello Cucinelli',
-    shoes: [
-      {name: 'tasselled black low-top lace-up', price: 1000},
-      {name: 'tasselled green low-top lace-up', price: 1100},
-      {name: 'plain beige suede moccasin', price: 950},
-      {name: 'plain olive suede moccasin', price: 1050}
-    ]
-  },
-  {
-    name: 'Gucci',
-    shoes: [
-      {name: 'red leather laced sneakers', price: 800},
-      {name: 'black leather laced sneakers', price: 900}
-    ]
-  }
-];
+// }
+// var currentInventory = [
+//   {
+//     name: 'Brunello Cucinelli',
+//     shoes: [
+//       {name: 'tasselled black low-top lace-up', price: 1000},
+//       {name: 'tasselled green low-top lace-up', price: 1100},
+//       {name: 'plain beige suede moccasin', price: 950},
+//       {name: 'plain olive suede moccasin', price: 1050}
+//     ]
+//   },
+//   {
+//     name: 'Gucci',
+//     shoes: [
+//       {name: 'red leather laced sneakers', price: 800},
+//       {name: 'black leather laced sneakers', price: 900}
+//     ]
+//   }
+// ];
 // Output
 // [ [ 'Brunello Cucinelli', 'tasselled black low-top lace-up', 1000 ],
 //   [ 'Brunello Cucinelli', 'tasselled green low-top lace-up', 1100 ],
@@ -154,40 +196,4 @@ var currentInventory = [
 
 
 
-console.log(renderInventory(currentInventory))
-
-
-// -------------------------------------------------
-// Bonus Round 3
-// Extract name, price values and populate them into an array
-// @param {Array} array of objects
-// @return {Array} array of arrays
-function renderInventory(inventory) {
-  //your code here
-}
-var currentInventory = [
-{
-name: 'Brunello Cucinelli',
-shoes: [
-  {name: 'tasselled black low-top lace-up', price: 1000},
-  {name: 'tasselled green low-top lace-up', price: 1100},
-  {name: 'plain beige suede moccasin', price: 950},
-  {name: 'plain olive suede moccasin', price: 1050}
-]
-},
-{
-name: 'Gucci',
-shoes: [
-  {name: 'red leather laced sneakers', price: 800},
-  {name: 'black leather laced sneakers', price: 900}
-]
-}
-];
-Output
-[ [ 'Brunello Cucinelli', 'tasselled black low-top lace-up', 1000 ],
-[ 'Brunello Cucinelli', 'tasselled green low-top lace-up', 1100 ],
-[ 'Brunello Cucinelli', 'plain beige suede moccasin', 950 ],
-[ 'Brunello Cucinelli', 'plain olive suede moccasin', 1050 ],
-[ 'Gucci', 'red leather laced sneakers', 800 ],
-[ 'Gucci', 'black leather laced sneakers', 900 ] ]
-console.log(renderInventory(currentInventory))
+// console.log(renderInventory(currentInventory))
